2922d55fd20587d75dc03df187c179b059f28efa
= <nfazzio@gmail.com>
2013-11-17 17:51:16 -0500

    finished required problems

--- week2/mlclass-ex1/gradientDescent.m
+++ week2/mlclass-ex1/gradientDescent.m
@@ -17,12 +17,12 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
-
+	% theta1 = theta(1) - alpha * (1/m) * (transpose(X * theta - y) * X(:,1));
+	% theta2 = theta(2) - alpha * (1/m) * (transpose(X * theta - y) * X(:,2));
+	theta = theta - transpose(alpha * (1/m) * (transpose(X * theta - y) * X));
+	% theta(1) = theta1;
+	% theta(2) = theta2;
+	
     % ============================================================
 
     % Save the cost J in every iteration    
