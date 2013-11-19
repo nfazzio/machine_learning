ca4aa5760a01e5d0b470a44b74157b8e772706b9
= <nfazzio@gmail.com>
2013-11-19 14:22:17 -0500

    Homework works 100%

--- week2/mlclass-ex1/gradientDescentMulti.m
+++ week2/mlclass-ex1/gradientDescentMulti.m
@@ -17,13 +17,7 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
-
-
-
-
-
-
+    theta = theta - transpose(alpha * (1/m) * (transpose(X * theta - y) * X));
 
 
 
