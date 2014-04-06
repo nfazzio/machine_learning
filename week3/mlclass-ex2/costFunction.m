17d71891695fdbc1e5e302c2e79f958850a89067
= <nfazzio@gmail.com>
2014-04-06 17:31:09 -0500

    added line that calculated h_theta which I had accidentally deleted

--- week3/mlclass-ex2/costFunction.m
+++ week3/mlclass-ex2/costFunction.m
@@ -22,6 +22,8 @@ grad = zeros(size(theta));
 
 % ========================= Compute J =========================
 
+h_theta = sigmoid(X * theta)
+transpose(-y) * log(h_theta)
 J = (1 / m) * sum(transpose(-y) * log(h_theta) - transpose((1 - y)) * log(1 - h_theta));
 
 % ======================== Compute grad =======================
