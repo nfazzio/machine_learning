a694a1391f33a0227aeda922d7ff0bb79a660b08
= <nfazzio@gmail.com>
2014-04-06 14:50:19 -0500

    moved scalar sigmoid subfunction into first parameter of arrayfun

--- week3/mlclass-ex2/sigmoid.m
+++ week3/mlclass-ex2/sigmoid.m
@@ -9,13 +9,9 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
-% Function to compute sigmoid of scalar
-function h = sigmoid_scalar(w)
-    h = 1 / (1 + e^-w);
-endfunction
 
 % Apply scalar sigmoid function to all element of a matrix or vector.
-g = arrayfun(@sigmoid_scalar, z);
+g = arrayfun(@(w)1 / (1 + e^-w), z);
 
 
 % =============================================================
