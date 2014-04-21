632ca6d6ae52ba7aecf67317097223694b2a12dd
= <nfazzio@gmail.com>
2014-04-20 20:18:34 -0500

    Completed regularization for backpropagation

--- week5/mlclass-ex4/nnCostFunction.m
+++ week5/mlclass-ex4/nnCostFunction.m
@@ -96,6 +96,11 @@ Delta1 = transpose(delta2) * a1;
 Theta2_grad = (1/m) * Delta2;
 Theta1_grad = (1/m) * Delta1;
 
+%Normalize gradients
+Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + ...
+                       (lambda / m) * Theta1(:,2:end)
+Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + ...
+                       (lambda / m) * Theta2(:,2:end)
 % ============================ Part 2 ========================
 
 % Create Regularizer
