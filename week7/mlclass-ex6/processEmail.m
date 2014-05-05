14aa3ce399b158c9bcd035751205c5dc9e52a0dd
= <nfazzio@gmail.com>
2014-05-04 21:33:40 -0500

    Completed and submitted processEmail.m

--- week7/mlclass-ex6/processEmail.m
+++ week7/mlclass-ex6/processEmail.m
@@ -97,12 +97,9 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
-
+    if find(strcmp(vocabList, str)),
+        str_loc = find(strcmp(vocabList, str));
+        word_indices = [word_indices ; str_loc];
 
 
 
